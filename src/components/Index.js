8a8bfa6889fc97fe7f87311063238604890bd987
Baptiste <baptiste.gauthier@synergiefamily.com>
2022-03-29 15:36:03 +0200

    Ajout footer

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Section1 from './landing/Section_1';
 import Section4 from './landing/Section_4';
+import {Footer} from './Footer';
 
 function Index() {
     return (
     <div>
         <Section1 />
         <Section4 />
+        <Footer />
     </div>
     )
 }
