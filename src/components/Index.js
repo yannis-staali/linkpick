1ec8f20b06e589a1bd4097222608aa3800e52b0f
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2022-03-29 23:21:57 +0200

    ajout section 2 ok

--- src/components/Index.js
+++ src/components/Index.js
@@ -4,12 +4,13 @@ import EnQuelquesClics from './EnQuelquesClics/EnQuelquesClicks'
 import Section3 from './landing/Section3';
 import Section4 from './landing/Section_4';
 import {Footer} from './Footer';
+import Section_2 from './landing/Section_2';
 
 function Index() {
     return (
     <div>
         <Section1/>
-        <EnQuelquesClics/>
+        <Section_2/>
         <Section3/>
         <Section4/>
         <Footer/>
---
+++ src/components/landing/Section_2.js
@@ -0,0 +1,22 @@
+import EnQuelquesClicks from "../EnQuelquesClics/EnQuelquesClicks"
+import './section_2.css'
+
+
+
+const Section_2 = ()=>{
+
+
+    return(
+
+        <div className="container_section_2">
+            <div  className="container_en_qq_clics">
+                <EnQuelquesClicks/>
+            </div>
+        </div>
+    )
+
+
+}
+
+
+export default Section_2
