81570d8450d4c87c9e17d80cc4ebe833f5a7d464
gaspard-lonchampt <gaspard.lonchampt@laplateforme.io>
2022-03-30 14:48:10 +0200

    [Fix] - rebase

---
+++ src/components/SolutionEcole/Section2.js
@@ -0,0 +1,14 @@
+import './Section1.css'
+import EnQuelquesClicks from '../EnQuelquesClics/EnQuelquesClicks'
+
+const Section2 = () => {
+
+    return (
+        <div className="section1">
+            <EnQuelquesClicks />
+        </div>
+    )
+}
+
+
+export default Section2
---
+++ src/components/SolutionEcole/index.js
@@ -0,0 +1,15 @@
+import Section1 from './Section1'
+import Section2 from './Section2'
+
+const SolutionEcole = () => {
+
+    return (
+        <div>
+            <Section1 />
+            <Section2 />
+        </div>
+    )
+}
+
+
+export default SolutionEcole
