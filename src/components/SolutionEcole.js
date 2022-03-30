075dc33361407df8e1533c01a63149374b13a13c
Baptiste <baptiste.gauthier@synergiefamily.com>
2022-03-30 12:14:55 +0200

    ok pour merge

--- src/components/SolutionEcole.js
+++ src/components/SolutionEcole.js
@@ -1,7 +1,4 @@
 
-
-import Form from "./Form"
-
 const SolutionEcole = () =>{
 
     return(
@@ -13,7 +10,6 @@ const SolutionEcole = () =>{
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nostrum eum possimus dolorum nihil cumque optio ab inventore accusantium maxime, rem repudiandae, culpa veniam distinctio perferendis veritatis impedit illo. Ex.
           Esse ad dicta laudantium repellat repudiandae distinctio at ipsam, laborum molestiae, aliquam molestias aspernatur veniam pariatur qui fugiat impedit quos ab eius architecto perferendis. Molestiae nisi debitis numquam ea iste?
           </p>
-          <Form />
         </div>
     )
 }
